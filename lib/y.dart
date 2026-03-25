fb0b9622720d518109e5d0f79af498a1e2e78b0f
Asadbek <asadbekomonaliyev4@gmail.com>
2026-03-25 20:03:58 +0500

    d

---
+++ lib/y.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class nimadir extends StatelessWidget {
+  const nimadir({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
